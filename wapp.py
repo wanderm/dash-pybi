4c6a50abd8b2becb19717de346fd15c9d0f9de12
Wander Mendes <wander.mendes@gmail.com>
2020-10-24 18:33:34 -0300

    20201024

--- wapp.py
+++ wapp.py
@@ -485,7 +485,7 @@ optionList = [{'label':'All', 'value':'All'}] + optionList
 
 # region Data
 
-df = pd.read_csv("../../Databases/br.csv")  # data from https://simplemaps.com/data/us-cities
+df = pd.read_csv("./wraw/br.csv")  # data from https://simplemaps.com/data/us-cities
 color_prop = 'population'
 
 def get_data(state):
